974bc452179ecbf21901e4591dd87fac5b7116fd
taexedo <vasildudinski@gmail.com>
2024-03-09 23:08:14 +0200

    Fix calculator's flow + history deletion and usability.

--- backend/public/server/main.server.mjs
+++ backend/public/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-KVHJ7DAL.mjs";import"./chunk-U25Q4SKE.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-CAX3ERWW.mjs";import"./chunk-U25Q4SKE.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
